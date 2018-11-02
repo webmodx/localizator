9cca108c24abea6b04bf26005e34e3e6d09a6e3e
nizart91 <nizart91@mail.ru>
2018-11-02 10:38:33 +0500

    Update build.config.php

--- _build/build.config.php
+++ _build/build.config.php
@@ -34,7 +34,7 @@ define('BUILD_MENU_UPDATE', true);
 define('BUILD_SETTING_UPDATE', false);
 //define('BUILD_CHUNK_UPDATE', false);
 
-//define('BUILD_SNIPPET_UPDATE', true);
+define('BUILD_SNIPPET_UPDATE', true);
 define('BUILD_PLUGIN_UPDATE', true);
 //define('BUILD_EVENT_UPDATE', true);
 //define('BUILD_POLICY_UPDATE', true);
