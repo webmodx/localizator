f45402c88b7c22cd8c0523f7be157ad1c56f7295
Ivan Bochkarev <ivanx86@gmail.com>
2019-04-16 17:46:35 +0600

    Adding French Translation

---
+++ core/components/localizator/lexicon/fr/setting.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+$_lang['area_localizator_main'] = 'Base';
+$_lang['area_localizator_translator'] = 'Langue cible';
+
+$_lang['setting_localizator_default_language'] = 'La clé de localisation par défaut';
+$_lang['setting_localizator_default_language_desc'] = 'Localisation de clé pour la version de langue principale';
+
+$_lang['setting_localizator_key_yandex'] = 'Clé d’API pour Yandex';
+$_lang['setting_localizator_key_yandex_desc'] = 'Clé d’API pour Yandex traducteur, https://translate.yandex.ru/developers/keys';
+
+$_lang['setting_localizator_default_translator'] = 'Traduction automatique de traducteur';
+$_lang['setting_localizator_key_yandex_desc'] = 'Jusqu\'à présent disponible uniquement pour Yandex';
+
+$_lang['setting_localizator_translate_translated'] = 'Localisation de complément traduit ?';
+$_lang['setting_localizator_translate_translated_desc'] = 'Lorsque vous utilisez automatique traduction traduire les champs vides de localisations existantes';
+
+$_lang['setting_localizator_translate_translated_fields'] = 'Écraser la localisation traduite ?';
+$_lang['setting_localizator_translate_translated_fields_desc'] = 'Lorsque vous utilisez automatique traduction écrase tous les champs lokalizaciii';
+
+$_lang['setting_localizator_translate_fields'] = 'Liste des champs pour la traduction';
+$_lang['setting_localizator_translate_fields_desc'] = 'Ces champs seront traduits en utilisant la traduction automatique';
+
+$_lang['setting_localizator_tv_fields'] = 'Liste des champs supplémentaires (TV)';
+$_lang['setting_localizator_tv_fields_desc'] = 'Ces champs supplémentaires seront disponibles pour l’édition dans la localisation. Laissez le paramètre vide, si vous avez besoin de tous les champs supplémentaires';
