feb327b2fdfba1e281121f3d467272b2b373b463
Ibochkarev <ivanx86@gmail.com>
2020-05-13 16:43:54 +0600

    Added Belarusian translation

---
+++ core/components/localizator/lexicon/be/default.inc.php
@@ -0,0 +1,67 @@
+<?php
+include_once 'setting.inc.php';
+
+$_lang['localizator'] = 'лакалізатар';
+$_lang['localizator_tab'] = 'Лакалізацыя';
+$_lang['localizator_menu_desc'] = 'Лакалізацыя сайта';
+$_lang['localizator_intro_msg'] = 'Вы можаце выбраць некалькі строчак, выкарыстаючы Shift або Ctrl.';
+$_lang['localizator_languages'] = 'Лакалізацыя';
+
+$_lang['localizator_id'] = 'Id';
+$_lang['localizator_key'] = 'Клавіша
+';
+$_lang['localizator_pagetitle'] = 'Загаловак';
+$_lang['localizator_longtitle'] = 'Дададковы загаловак';
+$_lang['localizator_menutitle'] = 'Меню заголоўка';
+$_lang['localizator_longtitle'] = 'Дададковы загаловак';
+$_lang['localizator_seotitle'] = 'ПА Загаловак';
+$_lang['localizator_description'] = 'ПА Апісанне';
+$_lang['localizator_keywords'] = 'ПА Ключавыя словы';
+$_lang['localizator_introtext'] = 'Абстрактна';
+$_lang['localizator__key'] = 'Ключ (хост)';
+$_lang['localizator_active'] = 'Актыўна';
+$_lang['localizator_language'] = 'Мова';
+$_lang['localizator_translate'] = 'Аўтаматычны пераклад';
+$_lang['localizator_translate_confirm'] = 'Вы сур\'ёза жадаеце выканаць аўтаматычны пераклад? Гэтае дзеянне можа заняць доўгі час.';
+$_lang['localizator_translate_wait'] = 'Аўтаматычны пераклад';
+$_lang['localizator_translate_wait_ext'] = 'Перакладзена...';
+$_lang['localizator_add'] = 'Дадаць пераклад';
+$_lang['localizator_error'] = 'Памылка';
+
+$_lang['localizator_language_create'] = 'Дадаць лакалізацыю';
+$_lang['localizator_language_update'] = 'Аднавіць лакалізацыю';
+$_lang['localizator_language_key'] = 'Ключавая лакалізацыя';
+$_lang['localizator_language_name'] = 'Імя';
+$_lang['localizator_language_cultureKey'] = 'Моўны ключ';
+$_lang['localizator_language_http_host'] = 'HTTP HOST';
+$_lang['localizator_language_description'] = 'Апісанне';
+$_lang['localizator_language_err_no_key'] = 'Не паказа лакалізацыя ключа';
+$_lang['localizator_language_err_key_exist'] = 'Ключ лакалізацыі ўжо выкарыстоўваецца';
+$_lang['localizator_language_err_no_http_host'] = 'Не паказан хост HTTP';
+$_lang['localizator_language_err_http_host_exist'] = 'Хост HTTP ужо выкарыстоўваецца';
+
+$_lang['localizator_lexicon'] = 'Слоўнік';
+$_lang['localizator_lexicon_create'] = 'Дадаць пераклад';
+$_lang['localizator_lexicon_name'] = 'Імя (ключ)';
+$_lang['localizator_lexicon_language'] = 'Мова';
+$_lang['localizator_lexicon_value'] = 'Велічына';
+
+$_lang['localizator_item_create'] = 'Стварыць';
+$_lang['localizator_item_update'] = 'Змяніць';
+$_lang['localizator_item_enable'] = 'Даступна';
+$_lang['localizator_items_enable'] = 'Даступна';
+$_lang['localizator_item_disable'] = 'Выключыць ';
+$_lang['localizator_items_disable'] = 'Адключыць';
+$_lang['localizator_item_remove'] = 'Скасаваць';
+$_lang['localizator_items_remove'] = 'Выдаліць';
+$_lang['localizator_item_remove_confirm'] = 'Вы ўпэўнены, што жадайце выдаліць гэты запіс?';
+$_lang['localizator_items_remove_confirm'] = 'Вы ўпэўнены, што жадайце выдаліць гэтыя запісы?';
+$_lang['localizator_item_active'] = 'Даступна';
+
+$_lang['localizator_item_err_nf'] = 'Запіс не знойдзена';
+$_lang['localizator_item_err_ns'] = 'Не паказан запіс';
+$_lang['localizator_item_err_remove'] = 'Не паказан запіс';
+$_lang['localizator_item_err_save'] = 'Захаванне не ўдалося';
+
+$_lang['localizator_grid_search'] = 'Пошук';
+$_lang['localizator_grid_actions'] = 'Дзеянні';
---
+++ core/components/localizator/lexicon/be/setting.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+$_lang['area_localizator_main'] = 'Базавы';
+$_lang['area_localizator_translator'] = 'Перакладзена';
+
+$_lang['setting_localizator_default_language'] = 'Ключ лакалізацыі па змаўчанні';
+$_lang['setting_localizator_default_language_desc'] = 'Ключ лакалізацыі для выбранай моўнай версіі';
+
+$_lang['setting_localizator_key_yandex'] = 'API ключ для Яндекса';
+$_lang['setting_localizator_key_yandex_desc'] = 'API ключ для Яндекс.Перакладчыка, https://translate.yandex.ru/developers/keys';
+
+$_lang['setting_localizator_default_translator'] = 'Аўтаматычны пераклад';
+$_lang['setting_localizator_key_yandex_desc'] = 'Даступна толькі для Яндекс';
+
+$_lang['setting_localizator_translate_translated'] = 'Дапоўніць перакладзеную лакалізацыю?';
+$_lang['setting_localizator_translate_translated_desc'] = 'Пры выкарыстанні аўтаматычнага перакладу перакладаеце пустыя палі з існуючых лакалізацый';
+
+$_lang['setting_localizator_translate_translated_fields'] = 'Перапісаць перакладзеную лакалізацыю?';
+$_lang['setting_localizator_translate_translated_fields_desc'] = 'Пры выкарыстанні аўтаматычнага перакладу адбываецца перазапіс ўсіх палёў лакалізацыі';
+
+$_lang['setting_localizator_translate_fields'] = 'Спіс палёў для перакладу';
+$_lang['setting_localizator_translate_fields_desc'] = 'Гэтыя палі будуць пераведзеныя Пры выкарыстанні аўтаматычнага перакладу';
+
+$_lang['setting_localizator_tv_fields'] = 'Спіс дадатковых палёў (ТБ)';
+$_lang['setting_localizator_tv_fields_desc'] = 'Гэтыя дадатковыя поля будуць даступныя для рэдагавання ў лакалізацыі. Пакіньце гэты параметр пустым, калі вам патрэбныя ўсе дадатковыя поля';
