4e225e82c847e067ad0e4026b05b725eaa2b8fdf
Igor Loktiev <goroskop@icloud.com>
2020-06-10 13:12:06 +0300

    mslocalizatoroptions
    Поддержка дополнительного компонента mslocalizatoroptions

--- core/components/msearch2/custom/filters/localizator.class.php
+++ core/components/msearch2/custom/filters/localizator.class.php
@@ -283,3 +283,99 @@ class mse2LocalizatorFilter extends mse2FiltersHandler {
 			: $this->buildParentsFilter($tmp, $name, 0);
 	}
 }
+
+
+	/**
+	 * Retrieves values from miniShop2 Product table
+	 *
+	 * @param array $keys Keys of ms2 products options
+	 * @param array $ids Ids of needed resources
+	 *
+	 * @return array Array with ms2 fields as keys and resources ids as values
+	 */
+	public function getMsOptionValues(array $keys, array $ids) {
+	    $this->modx->getService('msLocalizatorOptions', 'msLocalizatorOptions', MODX_CORE_PATH . 'components/mslocalizatoroptions/model/');
+		$filters = array();
+		$fields = $this->modx->getFields('msProductData');
+		$q = $this->modx->newQuery('msLocalizatorOption');
+		if (array_key_exists('sku_id', $fields)) {
+			$q->innerJoin('msProductData', 'Data', 'Data.sku_id = msLocalizatorOption.product_id');
+			$q->where(array('Data.id:IN' => $ids, 'key:IN' => $keys));
+			$q->select('Data.id as product_id, key, value');
+		}
+		else {
+			$q->where(array(
+				'product_id:IN' => $ids,
+				'key:IN' => $keys,
+				'localizator_key' => $this->modx->getOption('localizator_key', $this->config, $this->modx->localizator_key, true)
+			));
+			$q->select('product_id, key, value');
+		}
+		$tstart = microtime(true);
+		if ($q->prepare() && $q->stmt->execute()) {
+			$this->modx->queryTime += microtime(true) - $tstart;
+			$this->modx->executedQueries++;
+			while ($row = $q->stmt->fetch(PDO::FETCH_ASSOC)) {
+				$value = str_replace('"', '&quot;', trim($row['value']));
+				//if ($value == '') {continue;}
+				$key = strtolower($row['key']);
+				// Get ready for the special options in "key==value" format
+				if (strpos($value, '==')) {
+					list($key, $value) = explode('==', $value);
+					$key = preg_replace('/\s+/', '_', $key);
+				}
+				// --
+				if (isset($filters[$key][$value])) {
+					$filters[$key][$value][$row['product_id']] = $row['product_id'];
+				}
+				else {
+					$filters[$key][$value] = array($row['product_id'] => $row['product_id']);
+				}
+			}
+		}
+		else {
+			$this->modx->log(modX::LOG_LEVEL_ERROR, "[mSearch2] Error on get filter params.\nQuery: " . $q->toSQL() . "\nResponse: " . print_r($q->stmt->errorInfo(), 1));
+		}
+
+		return $filters;
+	}
+
+	/**
+	 * Prepares values for filter
+	 * Sort ms2 product options by order in its settings
+	 *
+	 * @param array $values IDs of resources
+	 * @param string $name Name of option
+	 *
+	 * @return array Prepared values
+	 */
+	public function buildOptionsFilter(array $values, $name = '') {
+	    $this->modx->getService('msLocalizatorOptions', 'msLocalizatorOptions', MODX_CORE_PATH . 'components/mslocalizatoroptions/model/');
+		$keys = array_keys($values);
+		if (empty($keys) || (count($keys) < 2 && empty($this->config['showEmptyFilters']))) {
+			return array();
+		}
+
+		$values = $this->buildDefaultFilter($values, $name);
+
+		/** @var msOption $option */
+		if ($option = $this->modx->getObject('msLocalizatorOption', array(
+			'key' => $name,
+			'localizator_key' => $this->modx->getOption('localizator_key', $this->config, $this->modx->localizator_key, true)))) {
+			if ($properties = $option->get('properties')) {
+				if (!empty($properties['values'])) {
+					$tmp = array();
+					foreach ($properties['values'] as $key) {
+						if (isset($values[$key])) {
+							$tmp[$key] = $values[$key];
+							unset($values[$key]);
+						}
+					}
+					$values = array_merge($tmp, $values);
+				}
+			}
+		}
+
+		return $this->sortFilters($values, 'options', array('name' => $name));
+	}
+} 
